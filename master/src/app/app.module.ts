954d689d7eb21019c50ae2618939b44e2a5c5b63
dotnet-code <@dotnet-code>
2023-06-05 15:03:45 +0530

    commit from code202306050345

--- master/src/app/app.module.ts
+++ master/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DesignatedPersonComponent } from './designated-person/designated-person.component';
+import { ComplianceOfficerListComponent } from './compliance-officer-list/compliance-officer-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DesignatedPersonComponent
+    DesignatedPersonComponent,
+    ComplianceOfficerListComponent
   ],
   imports: [
     BrowserModule,
---
+++ master/src/app/compliance-officer-list/compliance-officer-list.component.ts
@@ -0,0 +1,17 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-compliance-officer-list',
+  template: `
+    <h1>Compliance Officer List</h1>
+  `,
+  styles: []
+})
+export class ComplianceOfficerListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
